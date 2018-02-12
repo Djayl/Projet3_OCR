d10c7b0d062b48794339c9002b27ce43f799865a
DjelS <djilali.sakkar@gmail.com>
2018-02-12 23:06:28 +0100

    Modification de laclasse Character et ajouts de méthodes

--- GameOCR.swift
+++ GameOCR.swift
@@ -1,43 +1,95 @@
 // Creation of the class Character
 class Character{
+    
+    var fighterLife = 100
+    var fighterDamage = 10
+    
+    var magusLife = 50
+    var magusCure = 10
+    
+    var colossusLife = 120
+    var colossusDamage = 5
+    
+    var dwarfLife = 50
+    var dwarfDamage = 25
+    
+    func fighterAttackDwarf(){
+        dwarfLife -= fighterDamage
+        }
+    func fighterAttackColossus(){
+        colossusLife -= fighterDamage
+    }
+    func fighterAttackMagus(){
+        magusLife -= fighterDamage
+    }
+    func colossusAttackFighter(){
+        fighterLife -= colossusDamage
+    }
+    func colossusAttackDwarf(){
+        dwarfLife -= colossusDamage
+    }
+    func colossusAttackMagus(){
+        magusLife -= colossusDamage
+    }
+    func magusHealsFighter(){
+        if fighterLife > 0{
+            fighterLife += magusCure
+        }
+        else {
+            print("il est mort, je ne peux pas le réanimer")
+        }
+    }
+    func magusHealsDwarf(){
+        if dwarfLife > 0{
+            dwarfLife += magusCure
+        }
+        else {
+            print("il est mort, je ne peux pas le réanimer")
+        }
+    }
+    func magusHealsColossus(){
+        if colossusLife > 0{
+            colossusLife += magusCure
+        }
+        else {
+            print ("il est mort, je ne peux pas le réanimer")
+        }
+    }
+}
+
+
+// Creation of the class Fighter, the class of the character named Fighter
+class Fighter: Character{
     var fighterName: String
-    var magusName: String
-    var colossusName: String
-    var dwarfName: String
-    
-    
-    let numberOfCharacter = 3
-    
-    init(fighterName: String, magusName: String, colossusName: String, dwarfName: String) {
+    init (fighterName: String) {
         self.fighterName = fighterName
+    }
+}
+
+// Creation of the class Magus, the class of the character named Magus
+class Magus: Character{
+    var magusName: String
+    init (magusName: String){
         self.magusName = magusName
+    }
+    
+}
+
+// Creation of the class Colossus, the class of the character named Colossus
+class Colossus: Character{
+    var colossusName: String
+    init (colossusName: String){
         self.colossusName = colossusName
+    }
+    
+}
+
+// Creation of the class Dwarf, the class of the character named Dwarf
+class Dwarf: Character{
+    var dwarfName: String
+    init (dwarfName: String){
         self.dwarfName = dwarfName
     }
     
 }
 
-// Creation of the class Fighter, the class of the character named Fighter
-class Fighter : Character{
-    var fighterLife = 100
-    var fighterDamage = -10
-    
-}
-
-// Creation of the class Magus, the class of the character named Magus
-class Magus : Character{
-    var magusLife = 50
-    var magusCure = +10
-}
-
-// Creation of the class Colossus, the class of the character named Colossus
-class Colossus : Character{
-    var colossusLife = 120
-    var colossusDamage = -5
-}
-
-// Creation of the class Dwarf, the class of the character named Dwarf
-class Dwarf : Character{
-    var dwarfLife = 50
-    var dwarfDamage = -25
-}
