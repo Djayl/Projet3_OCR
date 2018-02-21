6cdaf0048d2bacc534bf44c9f97f0bcf60c3dd89
DjelS <djilali.sakkar@gmail.com>
2018-02-21 01:06:09 +0100

    modification de la classe Character avec de nouvelles méthodes

--- GameOCR.swift
+++ GameOCR.swift
@@ -1,62 +1,87 @@
 // Creation of the class Character
+
 class Character{
     
-    var fighterLife = 100
-    var fighterDamage = 10
+    var characterLife = ["fighter": 100, "magus": 50, "colossus": 150, "dwarf": 50]
+    var characterDamage = ["fighter": 10,"colossus": 5, "dwarf": 25]
+    let magusCure = 10
+    var comrade = ["Combattant", "Colosse", "Nain"]
+    var opponent : String
+    init (opponent: String){
+        self.opponent = opponent
+    }
     
-    var magusLife = 50
-    var magusCure = 10
     
-    var colossusLife = 120
-    var colossusDamage = 5
     
-    var dwarfLife = 50
-    var dwarfDamage = 25
+// The fighter attacks his opponents
     
-    func fighterAttackDwarf(){
-        dwarfLife -= fighterDamage
-        }
-    func fighterAttackColossus(){
-        colossusLife -= fighterDamage
-    }
-    func fighterAttackMagus(){
-        magusLife -= fighterDamage
-    }
-    func colossusAttackFighter(){
-        fighterLife -= colossusDamage
-    }
-    func colossusAttackDwarf(){
-        dwarfLife -= colossusDamage
-    }
-    func colossusAttackMagus(){
-        magusLife -= colossusDamage
-    }
-    func magusHealsFighter(){
-        if fighterLife > 0{
-            fighterLife += magusCure
-        }
-        else {
-            print("il est mort, je ne peux pas le réanimer")
+    func fighterAttack(){
+        if opponent == "Mage"{
+            (characterLife["magus"]!) -= (characterDamage["fighter"]!)
+            print("Vous attaquez le mage")
+        } else if opponent == "Colosse"{
+            (characterLife["colossus"]!) -= (characterDamage["fighter"]!)
+            print("Vous attaquez le colosse")
+        } else if opponent == "Nain"{
+            (characterLife["dwarf"]!) -= (characterDamage["fighter"]!)
+            print("Vous attaquez le nain")
+        } else {
+            print("Vous n'avez pas choisis d'adversaire")
         }
     }
-    func magusHealsDwarf(){
-        if dwarfLife > 0{
-            dwarfLife += magusCure
+// The colossus attacks his opponents
+    
+    func colossusAttack() {
+        if opponent == "Mage"{
+            (characterLife["magus"]!) -= (characterDamage["colossus"]!)
+            print("Vous attaquez le mage")
+        } else if opponent == "Combattant"{
+            (characterLife["fighter"]!) -= (characterDamage["colossus"]!)
+            print("Vous attaquez le combattant")
+        } else if opponent == "Nain"{
+            (characterLife["dwarf"]!) -= (characterDamage["colossus"]!)
+            print("Vous attaquez le nain")
+        } else {
+            print("Vous n'avez pas choisis d'adversaire")
         }
-        else {
-            print("il est mort, je ne peux pas le réanimer")
-        }
+        
     }
-    func magusHealsColossus(){
-        if colossusLife > 0{
-            colossusLife += magusCure
+// The Dwarf attacks his opponents
+    
+    func dwarfAttack() {
+        if opponent == "Mage"{
+            (characterLife["magus"]!) -= (characterDamage["dwarf"]!)
+            print("Vous attaquez le mage")
+        } else if opponent == "Colosse"{
+            (characterLife["colossus"]!) -= (characterDamage["dwarf"]!)
+            print("Vous attaquez le colosse")
+        } else if opponent == "Combattant"{
+            (characterLife["fighter"]!) -= (characterDamage["dwarf"]!)
+            print("Vous attaquez le combattant")
+        } else {
+            print("Vous n'avez pas choisis d'adversaire")
         }
-        else {
-            print ("il est mort, je ne peux pas le réanimer")
-        }
+        
     }
-}
+    
+// The magus heals his comrades
+    func magusHealing () {
+        if comrade == ["Combattant"] && (characterLife["fighter"]!) > 0 {
+            (characterLife["fighter"]!) += magusCure
+            
+        } else if comrade == ["Colosse"] && (characterLife["colossus"]!) > 0 {
+            (characterLife["colossus"]!) += magusCure
+            
+        } else if comrade == ["Nain"] && (characterLife["dwarf"]!) > 0 {
+            (characterLife["dwarf"]!) += magusCure
+            
+        } else {
+            print("Il est mort je ne peux pas le réanimer")
+        }
+        
+    }
 
+}
 
 // Creation of the class Fighter, the class of the character named Fighter
 class Fighter: Character{
