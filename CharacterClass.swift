3c05b0d9dc4e5e537dcc2cc96bbe417dfca01ef7
DjelS <djilali.sakkar@gmail.com>
2018-03-04 17:07:23 +0100

    Suppression des enumérations pour n'en laisser qu'une seule sous le nom charactertype

--- CharacterClass.swift
+++ CharacterClass.swift
@@ -1,13 +1,7 @@
-// enumerations for the characters, whether they are opponents or assailants
+// Enumeration for the characters, whether they are opponents or assailants
 
-enum Opponent {
+enum CharacterType {
     case dwarf, colossus, magus, fighter
-}
-enum Assailant {
-    case dwarf, colossus, magus, fighter
-}
-enum Comrade {
-    case dwarf, colossus, fighter
 }
 
 // Creation of the class Character
@@ -20,65 +14,66 @@ class Character{
 
 // Creation of the attack function, one for all the characters    
     
-func attack (parametre1: Opponent, parametre2: Assailant){
     
-    if parametre1 == Opponent.magus && parametre2 == Assailant.fighter{
-    (characterLife["magus"]!) -= (characterDamage["fighter"]!)
-    print("Vous attaquez le mage")
-    }
-   
-    if parametre1 == Opponent.colossus && parametre2 == Assailant.fighter{
-    (characterLife["colossus"]!) -= (characterDamage["fighter"]!)
-    print("Vous attaquez le colosse")
-    }
-   
-    if parametre1 == Opponent.dwarf && parametre2 == Assailant.fighter{
-    (characterLife["dwarf"]!) -= (characterDamage["fighter"]!)
-    print("Vous attaquez le nain")
-    }
-    
-    if parametre1 == Opponent.magus && parametre2 == Assailant.colossus{
+    func attack (parametre1: CharacterType, parametre2: CharacterType){
+        
+        if parametre1 == CharacterType.magus && parametre2 == CharacterType.fighter{
+            (characterLife["magus"]!) -= (characterDamage["fighter"]!)
+            print("Vous attaquez le mage")
+        }
+        
+        if parametre1 == CharacterType.colossus && parametre2 == CharacterType.fighter{
+            (characterLife["colossus"]!) -= (characterDamage["fighter"]!)
+            print("Vous attaquez le colosse")
+        }
+        
+        if parametre1 == CharacterType.dwarf && parametre2 == CharacterType.fighter{
+            (characterLife["dwarf"]!) -= (characterDamage["fighter"]!)
+            print("Vous attaquez le nain")
+        }
+        
+        if parametre1 == CharacterType.magus && parametre2 == CharacterType.colossus{
             (characterLife["magus"]!) -= (characterDamage["colossus"]!)
             print("Vous attaquez le mage")
-    }
-    if parametre1 == Opponent.dwarf && parametre2 == Assailant.colossus{
+        }
+        if parametre1 == CharacterType.dwarf && parametre2 == CharacterType.colossus{
             (characterLife["fighter"]!) -= (characterDamage["colossus"]!)
             print("Vous attaquez le combattant")
-    }
-    if parametre1 == Opponent.fighter && parametre2 == Assailant.colossus{
+        }
+        if parametre1 == CharacterType.fighter && parametre2 == CharacterType.colossus{
             (characterLife["dwarf"]!) -= (characterDamage["colossus"]!)
             print("Vous attaquez le nain")
-    }
-    if parametre1 == Opponent.magus && parametre2 == Assailant.dwarf{
+        }
+        if parametre1 == CharacterType.magus && parametre2 == CharacterType.dwarf{
             (characterLife["magus"]!) -= (characterDamage["dwarf"]!)
             print("Vous attaquez le mage")
-    }
-    if parametre1 == Opponent.colossus && parametre2 == Assailant.dwarf{
+        }
+        if parametre1 == CharacterType.colossus && parametre2 == CharacterType.dwarf{
             (characterLife["colossus"]!) -= (characterDamage["dwarf"]!)
             print("Vous attaquez le colosse")
-    }
-    if parametre1 == Opponent.fighter && parametre2 == Assailant.dwarf{
+        }
+        if parametre1 == CharacterType.fighter && parametre2 == CharacterType.dwarf{
             (characterLife["fighter"]!) -= (characterDamage["dwarf"]!)
             print("Vous attaquez le combattant")
         }
-}
+    }
     
 // The magus heals his comrades
 
-    func magusHealing (parametre: Comrade) {
-        if parametre == Comrade.fighter && (characterLife["fighter"]!) > 0 {
+    func magusHealing (parametre: CharacterType) {
+        if parametre == CharacterType.fighter && (characterLife["fighter"]!) > 0 {
             (characterLife["fighter"]!) += magusCure
             
-        } else if parametre == Comrade.colossus && (characterLife["colossus"]!) > 0 {
+        } else if parametre == CharacterType.colossus && (characterLife["colossus"]!) > 0 {
             (characterLife["colossus"]!) += magusCure
             
-        } else if parametre == Comrade.dwarf && (characterLife["dwarf"]!) > 0 {
+        } else if parametre == CharacterType.dwarf && (characterLife["dwarf"]!) > 0 {
             (characterLife["dwarf"]!) += magusCure
             
         } else {
             print("Il est mort je ne peux pas le réanimer")
         }
-}
+    }
 }
 
 // Creation of the class Fighter, the class of the character named Fighter
