8f924c39772c40dc10d69eecb434aeb9b7b9799d
DjelS <djilali.sakkar@gmail.com>
2018-03-10 16:00:37 +0100

    Création de deux instances, playerOne et playerTwo

--- projet3ocr/projet3ocr/main.swift
+++ projet3ocr/projet3ocr/main.swift
@@ -2,3 +2,9 @@
 print("Bonjour joueur 1, entrez votre nom:")
 var playerOne = Game()
 playerOne.playerCharacterChoice(playerIndex:1)
+
+print ("Bonjour joueur 2,entrez votre nom:")
+var playerTwo = Game()
+playerTwo.playerCharacterChoice(playerIndex:2)
+
+
