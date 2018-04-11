a3a9284cd157464d066132ac5ee62caed01e1182
DjelS <djilali.sakkar@gmail.com>
2018-04-11 19:10:28 +0200

    Appel de la méthode runGame pour les deux joueurs

--- projet3ocr/projet3ocr/main.swift
+++ projet3ocr/projet3ocr/main.swift
@@ -4,5 +4,5 @@ print("Bonjour joueur 1, entrez votre nom:")
 game.playerCharacterChoice(playerIndex:1)
 print ("Bonjour joueur 2,entrez votre nom:")
 game.playerCharacterChoice(playerIndex:2)
-
-
+game.runGame(playerIndex: 1)
+game.runGame(playerIndex: 2)
